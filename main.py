f512626d8158fc8aae6cf2c42c3ecc521a1d6154
TheRunaway <shadowangel404@proton.me>
2023-08-20 21:16:41 +0200

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
+def on_button_pressed_a():
+    global number
+    if number:
+        number = False
+    else:
+        number = True
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global sensor, number
+    sensor += 1
+    sensor = sensor % 5
+    number = False
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+number = False
+sensor = 0
 sensor = 0
 number = True
 height = 0
 
 def on_forever():
+    global height
     if sensor == 0:
-        if sensor:
-            pass
+        if number:
+            height = input.temperature() * 5 / 50
         else:
             basic.show_string("" + str(input.temperature()) + "°C")
     elif sensor == 1:
+        if number:
+            height = input.light_level() * 5 / 255
+        else:
+            basic.show_string("" + str((input.light_level())))
+    elif sensor == input.sound_level():
         pass
-    elif sensor == 2:
+    elif sensor == input.compass_heading():
         pass
-    elif sensor == 3:
+    elif sensor == input.acceleration(Dimension.X):
         pass
-    elif sensor == 4:
+    if height <= 0:
+        pass
+    elif height == 1:
+        pass
+    elif height == 2:
+        pass
+    elif height == 3:
+        pass
+    elif height == 4:
+        pass
+    elif height == 5:
         pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
-let sensor = 0
+input.onButtonPressed(Button.A, function () {
+    if (number) {
+        number = false
+    } else {
+        number = true
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    sensor += 1
+    sensor = sensor % 5
+    number = false
+})
 let number = false
+let sensor = 0
+sensor = 0
+number = true
 let height = 0
 basic.forever(function () {
     if (sensor == 0) {
@@ -15,10 +29,71 @@ basic.forever(function () {
             basic.showString("" + (input.lightLevel()))
         }
     } else if (sensor == 2) {
-    	
+        if (number) {
+            height = input.soundLevel() * 5 / 255
+        } else {
+            basic.showString("" + (input.soundLevel()))
+        }
     } else if (sensor == 3) {
-    	
+        if (number) {
+            height = Math.abs(input.compassHeading()) * 5 / 180
+        } else {
+            basic.showString("" + (input.compassHeading()))
+        }
     } else if (sensor == 4) {
-    	
+        if (number) {
+            height = input.acceleration(Dimension.X) * 5 / 19.62
+        } else {
+            basic.showString("" + (input.acceleration(Dimension.X)))
+        }
+    }
+    if (height <= 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (height == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (height == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    } else if (height == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (height == 4) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (height >= 5) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
     }
 })
